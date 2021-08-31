535b6352a7532d7623907f9a877b2971497774d5
austindflatt <austindflatt@gmail.com>
2021-08-31 01:10:58 -0400

    installed dependencies and set up React chat engine

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine 
+            height="100vh"
+            projectID="552dcf38-7f64-4b97-8f27-9ff90bd402d7"
+            userName="Guest"
+            userSecret="123123"
+        />
+    )
+}
+
+export default App;
